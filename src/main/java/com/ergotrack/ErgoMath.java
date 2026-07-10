d2fde14620fb23ab35c2ac88bd5a80ef55179a70
Manikandan M <manimmaths@gmail.com>
2026-07-10 07:09:53 +0530

    Adding Comments

--- src/main/java/com/ergotrack/ErgoMath.java
+++ src/main/java/com/ergotrack/ErgoMath.java
@@ -2,6 +2,9 @@ package com.ergotrack;
 
 public class ErgoMath {
 
+    		// A simple inner class to hold coordinates - TreeNode structure
+		// We can go with manual implementation make the code fuzzy
+		
     public static class Point {
         double x;
         double y;
@@ -12,15 +15,22 @@ public class ErgoMath {
         }
     }
 
+    
+    		//Calculates the angle ABC at vertex B (the Hip)
+
     public static double calculateAngle(Point A, Point B, Point C) {
-        
+    	
+        // Calculate angles of vectors BA and BC relative to the X-axis
         double angleBA = Math.atan2(A.y - B.y, A.x - B.x);
         double angleBC = Math.atan2(C.y - B.y, C.x - B.x);
         
+        // Find the absolute difference in radians
         double radians = angleBA - angleBC;
         
+        // Convert radians to degrees
         double degrees = Math.abs(radians * 180.0 / Math.PI);
         
+        // We want the interior angle (always <= 180 degrees)
         if (degrees > 180.0) {
             degrees = 360.0 - degrees;
         }
