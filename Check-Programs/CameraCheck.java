123d74f46c58042bd23f84d86e94d3646ea8ea1f
Manikandan M <manimmaths@gmail.com>
2026-07-10 07:10:29 +0530

    Adding the commends

--- Check-Programs/CameraCheck.java
+++ Check-Programs/CameraCheck.java
@@ -1,6 +1,5 @@
 // Camera check file
 
-
 package com.ergotrack;
 
 import org.bytedeco.javacv.CanvasFrame;
@@ -8,31 +7,42 @@ import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.OpenCVFrameGrabber;
 
 public class CameraCheck {
-    public static void main(String[] args) {
-        // 1. Create a grabber object targeting your default webcam (0)
+
+	public static void main(String[] args) {
+		
+        //Create a grabber object targeting your default webcam (0)
         OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(0);
         
+        //Create a simple window window titled "ErgoTrack Window"        
+        CanvasFrame canvas = new CanvasFrame("ErgoTrack Demo");
+        canvas.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+
+        //Initialize the object for Frame as frame
+        Frame frame = new Frame();
+        
         try {
-            grabber.start(); // Open the webcam
+        		// Open the webcam
+            grabber.start();
             
-            // 2. Create a simple window window titled "ErgoTrack Window"
-            CanvasFrame canvas = new CanvasFrame("ErgoTrack Demo");
-            canvas.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
-            
-            // 3. A simple while loop (just like a LeetCode pointer problem!)
+            //While loop for infinite run that until the canvas frame is closed by the user
             while (canvas.isVisible()) {
+            	
                 // Grab a single frame from the webcam continuous stream
-                Frame frame = grabber.grab();
+            		frame = grabber.grab();
                 
-                if (frame != null) {
-                    // Display that frame in our window window
-                    canvas.showImage(frame);
-                }
+                // Display that frame in our window window
+            		if (frame != null) canvas.showImage(frame);
+            		
             }
             
             grabber.stop();
-        } catch (Exception e) {
+            grabber.close();
+            
+        }
+        catch (Exception e) {
             e.printStackTrace();
         }
+        
     }
+	
 }
